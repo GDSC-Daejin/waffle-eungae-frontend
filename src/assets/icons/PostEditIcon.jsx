bddd0c7ebd069d856454f6ca22271475729ce8f6
seojw0124 <wjddn2000124@gmail.com>
2023-03-01 15:37:04 +0900

    :sparkles: 포스트 세부 페이지 디자인 추가

---
+++ src/assets/icons/PostEditIcon.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const PostEditIcon = () => {
+  return (
+    <svg
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_10_84)">
+        <path
+          d="M3.14999 14.7879V17.85H6.21207L15.2431 8.81894L12.1811 5.75687L3.14999 14.7879ZM17.6112 6.45094C17.9296 6.13248 17.9296 5.61806 17.6112 5.2996L15.7004 3.38887C15.382 3.07041 14.8675 3.07041 14.5491 3.38887L13.0548 4.88316L16.1169 7.94523L17.6112 6.45094V6.45094Z"
+          fill="#6B7684"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_10_84">
+          <rect
+            width="15.75"
+            height="15.75"
+            fill="white"
+            transform="translate(2.625 2.625)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default PostEditIcon;
--- src/pages/PostDetail/styled.js
+++ src/pages/PostDetail/styled.js
@@ -1,11 +1,25 @@
 import styled from "styled-components";
 
 export const PostDetailContainer = styled.div`
-  width: 1120px;
-  margin: auto;
-  background: #61dafb;
+  position: relative;
+  z-index: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+  max-width: 1200px;
+  margin: 0px auto;
+  min-width: 320px;
+`;
+export const PostWrapper = styled.div`
+  padding: 0px 20px;
+  margin: 0px auto;
+  width: 100%;
+  height: 100%;
+  max-width: 800px;
 `;
 export const PostHead = styled.div`
+  font-size: 18px;
   display: flex;
   flex-direction: column;
   margin-top: 110px;
@@ -20,7 +34,8 @@ export const Category = styled.div`
   margin-bottom: 20px;
   width: fit-content;
 `;
-export const PostTitle = styled.div`
+export const PostTitle = styled.h1`
+  font-size: 32px;
   width: 100%;
   word-break: break-word;
   margin-bottom: 23px;
@@ -30,28 +45,28 @@ export const PostAuthorWrapper = styled.div`
   position: relative;
   display: flex;
   flex-direction: row;
+  margin-bottom: 50px;
 `;
 export const PostAuthor = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
+  margin-right: 10px;
 `;
-
 export const PostDate = styled.div`
-  position: relative;
+  color: #8b95a1;
+`;
+export const PostIconWrapper = styled.div`
+  position: absolute;
   display: flex;
   flex-direction: row;
+  gap: 5px;
+  right: 20px;
 `;
-export const PostEditIconWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
+export const PostIcon = styled.div`
+  cursor: pointer;
 `;
-export const PostTrashIconWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-`;
+/*export const PostTrashIconWrapper = styled.div`
+  position: absolute;
+  right: 0px;
+`;*/
 export const PostContent = styled.div`
   position: relative;
   display: flex;
