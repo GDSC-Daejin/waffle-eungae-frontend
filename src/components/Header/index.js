378b1673052a13ebe11abdffd38fe06968f09224
jinhye <jinh2376@gmail.com>
2023-03-23 18:44:10 +0900

    :bento: add logo
    logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import { Empty, HeaderBlock } from "./style";
+import { Empty, HeaderBlock, Logo } from "./style";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
     <HeaderBlock>
+      <Logo src={logo} alt="logo" />
       <NavLink to="/">홈</NavLink>
       <NavLink to="/natureinfo">환경 정보</NavLink>
       <NavLink to="/write">글쓰기</NavLink>
