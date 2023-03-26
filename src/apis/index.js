77e71276200e88e1e71cd42fcd9f6c130169bdb7
jinhye <jinh2376@gmail.com>
2023-03-26 23:14:30 +0900

    :alien: api 추가

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 export const getCategory = async () => {
-  return await axios.get(`https://eung-ae-back.kro.kr/`);
+  return await axios.get(`https://eung-ae-back.kro.kr/category`);
 };
 export const getPostASC = async () => {
-  return await axios.get(`https://eung-ae-back.kro.kr/post?sort=ASC`);
+  return await axios.get(`https://eung-ae-back.kro.kr?sort=ASC`);
 };
 export const getPostByCategoryASC = async (categoryId) => {
   return await axios.get(
-    `https://eung-ae-back.kro.kr/post/categoryPost/${categoryId}?sort=ASC`
+    `https://eung-ae-back.kro.kr/categoryPost/${categoryId}?sort=ASC`
   );
+};
+export const getViewCount = async () => {
+  return await axios.get(`https://eung-ae-back.kro.kr/viewCount?sort=ASC`);
 };
