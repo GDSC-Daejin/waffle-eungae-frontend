4157344726e92ea2dd037cec9c50c0a6c0843c82
jinhye <jinh2376@gmail.com>
2023-03-29 00:17:47 +0900

    :hammer: merge후 다듬기

--- src/styles/layout.js
+++ src/styles/layout.js
@@ -2,37 +2,18 @@ import styled from "styled-components";
 
 //보통 웹사이트에서 사용하는 Container 사이즈입니다.
 
-export const LayoutContainer = styled.div`
-  position: relative;
-<<<<<<< HEAD
-  width: 100%;
-=======
-  height: 700px;
-  max-width: 940px;
-`;
 export const LayoutContainers = styled.div`
   position: relative;
   width: 100%;
-  height: 100%;
-  max-width: 1140px;
-
->>>>>>> develop
   margin: 0 auto;
   min-width: 320px;
   flex: 1;
 `;
 export const ContainerInner = styled.div`
-<<<<<<< HEAD
-<<<<<<< HEAD
   width: 1050px;
-=======
-  width: 100%;
 `;
 export const ContainerInners = styled.div`
   width: 92%;
->>>>>>> develop
-=======
->>>>>>> 11187c54ebfe73a58b85e935caf4b195237df3e4
   height: 100%;
   max-width: 1050px;
   margin: 0 auto;
